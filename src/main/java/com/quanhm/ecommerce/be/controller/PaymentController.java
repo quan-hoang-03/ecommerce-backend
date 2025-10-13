c8012284caefedf3c4b0c636f0009e4094afe6df
Quân Hoàng <quanhoang789a@gmail.com>
2025-10-14 06:36:58 +0700

    config payload

--- src/main/java/com/quanhm/ecommerce/be/controller/PaymentController.java
+++ src/main/java/com/quanhm/ecommerce/be/controller/PaymentController.java
@@ -1,9 +1,11 @@
 package com.quanhm.ecommerce.be.controller;
 
+import lombok.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class PaymentController {
+//    @Value("")
 }
